0ae4ab0961f6008ebb9745520f1bea55bd1655c3
umeraltaf095 <umergujjar095@gmail.com>
2025-04-24 17:30:58 +0500

    Some changes at content js to clean up the code

--- content.js
+++ content.js
@@ -1,27 +1,28 @@
 console.log("content running successfully");
 
+const videos = document.querySelectorAll("video");
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
    // video pause feature
     if (request.action === "pauseVideo") {                               
-        const videos = document.querySelectorAll("video");
-        if (videos.length>0) {
+           if (videos.length>0) {
             videos.forEach((video) => {
                 video.pause();
               
-                console.log("Video paused successfully");
+              //  console.log("Video paused successfully");
             });
         } else {
             console.log("No video element found");
         }
     } else  if (request.action === "playVideo") {   
           
-      const videos = document.querySelectorAll("video");
+      
       if (videos.length>0) {
           videos.forEach((video) => {
             if(video.paused){
               video.play();
             
-              console.log("Video played successfully");
+             // console.log("Video played successfully");
             }
           });
       } else {
@@ -29,15 +30,13 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       }
   } // video backward feature  
   else if(request.action==="backwardVideo"){                            
-      const videos = document.querySelectorAll("video")
-      if(videos.length >0){
+       if(videos.length >0){
         videos.forEach((video)=>{
         video.currentTime -=10;
         
-        console.log("Video moves backward successfully", video.currentTime);
+       // console.log("Video moves backward successfully", video.currentTime);
         
-  
-        })
+    })
       }
       else{
         console.log("No video element found");
@@ -45,15 +44,13 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       }
     } // video forward feature 
     else  if(request.action==="forwardVideo"){                          
-      const videos = document.querySelectorAll("video")
       if(videos.length >0){
         videos.forEach((video)=>{
         video.currentTime +=10;
         
-        console.log("Video moves forward successfully", video.currentTime);
+       // console.log("Video moves forward successfully", video.currentTime);
         
-  
-        })
+   })
       }
       else{
         console.log("No video element found");
@@ -66,22 +63,21 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       const level = parseInt(request.action.replace("freqControl", ""));
       const freq = level * 1000; 
       bequadFilter.frequency.setValueAtTime(freq, audio.currentTime);
-      console.log(`Frequency changed to ${freq} Hz`);
+      //console.log(`Frequency changed to ${freq} Hz`);
     }  // Speed Control 
      else  if (request.action && request.action.startsWith("speedControl")) {      
       console.log("controller working properly");
       
       const level = parseInt(request.action.replace("speedControl", ""), 10);
       const speed =  Math.min(level*0.25, 2);
-      const videos = document.querySelectorAll('video');
-      if(videos.length>0){
+       if(videos.length>0){
         videos.forEach((video)=>{
           video.playbackRate = speed;
   
         })
       } // sound control
     } else  if(request.action && request.action.startsWith("soundControl")){                          
-      const videos = document.querySelectorAll('video');
+      
       const level = parseInt(request.action.replace("soundControl", " "),10);
       if(videos.length>0){
         videos.forEach((video)=>{
@@ -105,10 +101,9 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
  bequadFilter.type = 'bandpass';
  bequadFilter.frequency.value = 1000;
 
- const videos = document.querySelectorAll('video');
 if(videos.length>0){
-  videos.forEach((v)=>{
-    const source = audio.createMediaElementSource(v);
+  videos.forEach((video)=>{
+    const source = audio.createMediaElementSource(video);
     source.connect(bequadFilter);
     bequadFilter.connect(audio.destination);
 
